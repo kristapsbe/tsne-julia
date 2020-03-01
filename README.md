1f42e5b9f989009acd71acf523e7a13e8ed4f01f
Kristaps <kristaps@sannsyn.com>
2020-03-01 14:56:29 +0200

    updated some of the code and readme

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-an implementation of tsne in the julia programming language
+An (admittedly slow) implementation of the t-SNE dimensionality reduction algorithm in the [Julia programming laguage](https://julialang.org/).
+
+The code is based on the information available in the ["Visualizing Datausing t-SNE" research paper by Laurens van der Maaten and Geoffrey Hinton](http://www.jmlr.org/papers/volume9/vandermaaten08a/vandermaaten08a.pdf) and used the [Golang t-SNE implementation made by Daniel Salvadori](https://github.com/danaugrs/go-tsne) as a basis.
--- tsne.jl
+++ tsne.jl
@@ -6,10 +6,11 @@ using Random, Distributions
 using MLDatasets
 
 # the implementation is based on http://www.jmlr.org/papers/volume9/vandermaaten08a/vandermaaten08a.pdf
-# and https://raw.githubusercontent.com/danaugrs/go-tsne/master/tsne/tsne.go was used to understand some of the math and get some guidance on the general layout
+# and uses https://raw.githubusercontent.com/danaugrs/go-tsne/master/tsne/tsne.go as a basis
+# with input params borrowed from https://scikit-learn.org/stable/modules/generated/sklearn.manifold.TSNE.html
 
 # setting up constants that we'll need
-epsilon = 10^-7
+epsilon = 10^-7 # minimum gradient norm (if smaller optimization is stopped)
 fake_zero = 10^-12 # can't really have 0 for some of the calculations - making a mumber really close to it
 tol = 10^-5 # enthropy tolerance
 max_binary = 50 # max number of steps that the binary search gets to do
@@ -21,20 +22,21 @@ m = 5 # the number of dimensions
 m_out = 2 # the number of dimensions we want to get as an output
 T = 1000 # the number of iterations
 
-perp = 5 # in a sense, a guess about the number of close neighbors each point has
-l_rate = 100 # the learning rate
+perp = 30 # in a sense, a guess about the number of close neighbors each point has
+l_rate = 200 # the learning rate
+momentum = 0.5 # sklearns gradient descent internally uses this as the default val
 
 # creating our dummy dataset that'll look like
 # [[1, 2, 3, 4, 5, 6], [2, 3, 4, 5, 6, 7], [3, 4, 5, 6, 7, 8], [4, 5, 6, 7, 8, 9], [5, 6, 7, 8, 9, 10]]
-X = [[v for v in i:m+i] for i in 1:n]
-#println("Loading MNIST")
-#X = MNIST.convert2features(MNIST.traintensor())
-#m, n = size(X)
-#n = 2000 # limit the size for testing
+#X = [[v for v in i:m+i] for i in 1:n]
+println("Loading MNIST")
+X = MNIST.convert2features(MNIST.traintensor())
+m, n = size(X)
+n = 3000 # limit the size for testing (the thing's quite slow)
 colmap = ["#e6194b", "#3cb44b", "#ffe119", "#4363d8", "#f58231", "#911eb4", "#46f0f0", "#f032e6", "#bcf60c", "#fabebe", "#008080", "#e6beff", "#9a6324", "#fffac8", "#800000", "#aaffc3", "#808000", "#ffd8b1", "#000075", "#808080", "#ffffff", "#000000"]
 labels = MNIST.trainlabels() # need the labels to set up the colors
 cols = [labels[i]+1 for i in 1:n] #MNIST.trainlabels()[keys] # getting the corresponding lables so that we can color the scatterplot
-#X = [X[1+(m*(i-1)):(m*(i-1))+m] for i in 1:n] # this is probably inneficient as all hell, but makes thinking a lot easier for me
+X = [X[1+(m*(i-1)):(m*(i-1))+m] for i in 1:n] # this is probably inneficient as all hell, but makes thinking a lot easier for me
 println("MNIST loaded")
 
 # calculates the squared euclidean distance between two lists of the same length
@@ -51,7 +53,7 @@ function squared_distances(X, n, m)
   dists = [[Float64(0) for i in 1:n] for j in 1:n] 
   for i in 1:n
     for j in (i+1):n # skip the diagonal as well
-      tmp_dist = squared_euclidean(X[i], X[j], m)
+      tmp_dist = squared_euclidean(X[i], X[j], m) # and reuse the value for the pair
       dists[i][j] = tmp_dist
       dists[j][i] = tmp_dist
     end
@@ -85,7 +87,7 @@ function similarities(D, m, n, tol, perp, epsilon)
           p = exp(-D[i][k]*beta)
         end
 
-        P[i][k] = p
+        P[i][k] = p # note that we're not updating the whole thing (can't really do shorthand as a result)
         p_sum += p
       end
       
@@ -135,34 +137,21 @@ function similarities(D, m, n, tol, perp, epsilon)
 end
 
 
-function cost_gradient(P, Y, n, m_out, P_log_P, dCdY, fake_zero)
+function cost_gradient(P, Y, n, m_out, P_log_P, fake_zero)
+  # q i|j (1+dist^2)^-1 / sum(i≠j, (1+dist^2)^-1)
   Y_dist = squared_distances(Y, n, m_out)
-  Y_dist = [[((i != j) ? 1/(1+Y_dist[i][j]) : 0) for j in 1:n] for i in 1:n] # clear out the diagonal and replace values with the student t-distribution
-  # normalize the distance matrix
-  sum_q = sum(sum(Y_dist))
-  Q = [[max(Y_dist[i][j]/sum_q, fake_zero) for j in 1:n] for i in 1:n]
+  # set up the top part of that calculation
+  Q_top = [[((i != j) ? 1/(1+Y_dist[i][j]) : 0) for j in 1:n] for i in 1:n] 
+  # get the bottom bit of that function
+  sum_q = sum(sum(Q_top))
+  # build up the q i|j set
+  Q = [[max(Q_top[i][j]/sum_q, fake_zero) for j in 1:n] for i in 1:n]
   # the non-constant portion of the divergence
-  Q_log = [[log(Y_dist[i][j]) for j in 1:n] for i in 1:n]
-  # compute the divergence
+  Q_log = [[log(Q[i][j]) for j in 1:n] for i in 1:n]
+  # compute the Kullback-Leiber divergence
   P_log_Q = sum(sum([[P[i][j]*Q_log[i][j] for j in 1:n] for i in 1:n]))
-  divergence = P_log_P-P_log_Q
-  # compute the matrix of scalar multiple for scaling the 3d difference matrix prior to squashing
-  mult = P.-Q
-  mult = mult./4
-  mult = [[mult[i][j]*Y_dist[i][j] for j in 1:n] for i in 1:n]
-  # compute gradient
-  for r in 1:n
-    for c in 1:n
-      m = mult[r][c]
-      for k in 1:m_out
-        y_diff = Y[r][k]-Y[c][k]
-        orig = dCdY[r][k]
-        dCdY[r][k] = orig+m*y_diff
-      end
-    end
-  end
-
-  return divergence, dCdY
+  # work out the gradient of the Kullback-Leibler divergence between P and the Student-t based joint probability distribution Q
+  return [4*sum([(P[i][j]-Q[i][j])*(Y[i]-Y[j])/(1+Y_dist[i][j]) for j in 1:n]) for i in 1:n]
 end
 
 
@@ -178,9 +167,8 @@ println("Similarities adjusted")
 # sample initial solution Y(0)={y1, y2, ..., yn} for N(0,10^-4*I)
 init_vals = rand(Normal(0, init_stdev), m_out*n)
 Y = [[init_vals[m_out*i-j+1] for j in 1:m_out] for i in 1:n]
+Y_2 = Y # the Y(t-2) set that is to be used for the momentum calculations
 println("Y initialised")
-# set up the gradien matrix
-dCdY = [[Float64(0) for j in 1:m_out] for i in 1:n]
 # compute and store the constant portion of the KL divergence
 P_log_P = sum(sum([[log(P[i][j])*P[i][j] for i in 1:n] for j in 1:n]))
 # for t=1 to T do
@@ -188,15 +176,16 @@ P_log_P = sum(sum([[log(P[i][j])*P[i][j] for i in 1:n] for j in 1:n]))
 #   where T is the number of epochs that we're looking to work through
 println("Training")
 for t in 1:T
-  global dCdY, P, Y, n, m_out, P_log_P, fake_zero, cols
-  # compute KL divergence and update the gradient matrix
-  divergence, dCdY = cost_gradient(P, Y, n, m_out, P_log_P, dCdY, fake_zero)
-  # step in the direction of negative gradient (times the learning rate)
-  Y = dCdY.*l_rate
-  # reproject Y to have zero mean
-  y_mean = [sum([Y[j][i] for j in 1:n]) for i in 1:m_out]
-  Y = [[Y[i][j]-y_mean[j] for j in 1:m_out] for i in 1:n]
+  global P, Y, Y_2, n, m_out, P_log_P, fake_zero, cols, momentum
+  # compute the gradient matrix
+  dCdY = cost_gradient(P, Y, n, m_out, P_log_P, fake_zero)
+  # saving the Y(t-1) matrix for the time being
+  Y_1 = Y
+  # step in the direction of the gradient (times the learning rate)
+  Y += dCdY.*l_rate+(Y-Y_2).*momentum
 
+  # Y has been updated - our Y(t-1) can now be used as Y(t-2) for the next iter
+  Y_2 = Y_1
   println(t)
   if t%10 == 0 # keeping myself from getting swamped in charts
     xs = [[Y[i][1]] for i in 1:n]
