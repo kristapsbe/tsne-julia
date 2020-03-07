a2c295eca463317575671bc703079ca1f87d686a
Kristaps <kristaps@sannsyn.com>
2020-03-07 04:55:29 +0200

    updated everything to use matrices instead of lists of lists

--- tsne.jl
+++ tsne.jl
@@ -1,6 +1,6 @@
-# TODO: i cannot for the life of me remember which package was i supposed to add for MLDatasets
-import Pkg; Pkg.add("Distributions"); Pkg.add("Plots"); Pkg.add("MLDatasets")
+import Pkg; Pkg.add("Distributions"); Pkg.add("Plots"); Pkg.add("MLDatasets"); Pkg.add("CSV")
 
+using CSV
 using Plots
 using Random, Distributions
 using MLDatasets
@@ -17,27 +17,19 @@ max_binary = 50 # max number of steps that the binary search gets to do
 init_stdev = 10^-4 # the stdev that we're initialising Y with
 
 # and init parameters that we'll be working with
-n = 50 # the number of entries that we're working with
-m = 5 # the number of dimensions
 m_out = 2 # the number of dimensions we want to get as an output
 T = 1000 # the number of iterations
 
-perp = 30 # in a sense, a guess about the number of close neighbors each point has
-l_rate = 200 # the learning rate
-momentum = 0.5 # sklearns gradient descent internally uses this as the default val
+perp = 50 # in a sense, a guess about the number of close neighbors each point has
+l_rate = 300 # the learning rate
 
-# creating our dummy dataset that'll look like
-# [[1, 2, 3, 4, 5, 6], [2, 3, 4, 5, 6, 7], [3, 4, 5, 6, 7, 8], [4, 5, 6, 7, 8, 9], [5, 6, 7, 8, 9, 10]]
-#X = [[v for v in i:m+i] for i in 1:n]
-println("Loading MNIST")
-X = MNIST.convert2features(MNIST.traintensor())
-m, n = size(X)
-n = 3000 # limit the size for testing (the thing's quite slow)
+# loading our dataset
+csv_data = CSV.read("tsne-julia/abalone.csv", header=false)
+X = convert(Matrix, csv_data[:, 2:9])
+n, m = size(X)
+cols = convert(Matrix, csv_data[:, 1:1])
+unique_cols = unique(cols)
 colmap = ["#e6194b", "#3cb44b", "#ffe119", "#4363d8", "#f58231", "#911eb4", "#46f0f0", "#f032e6", "#bcf60c", "#fabebe", "#008080", "#e6beff", "#9a6324", "#fffac8", "#800000", "#aaffc3", "#808000", "#ffd8b1", "#000075", "#808080", "#ffffff", "#000000"]
-labels = MNIST.trainlabels() # need the labels to set up the colors
-cols = [labels[i]+1 for i in 1:n] #MNIST.trainlabels()[keys] # getting the corresponding lables so that we can color the scatterplot
-X = [X[1+(m*(i-1)):(m*(i-1))+m] for i in 1:n] # this is probably inneficient as all hell, but makes thinking a lot easier for me
-println("MNIST loaded")
 
 # calculates the squared euclidean distance between two lists of the same length
 function squared_euclidean(p, q, m)
@@ -50,12 +42,12 @@ end
 
 # calculates a squared distance matrix for row vectors in X
 function squared_distances(X, n, m) 
-  dists = [[Float64(0) for i in 1:n] for j in 1:n] 
+  dists = zeros(n, n)
   for i in 1:n
     for j in (i+1):n # skip the diagonal as well
-      tmp_dist = squared_euclidean(X[i], X[j], m) # and reuse the value for the pair
-      dists[i][j] = tmp_dist
-      dists[j][i] = tmp_dist
+      tmp_dist = squared_euclidean(X[i,:], X[j,:], m) # and reuse the value for the pair
+      dists[i, j] = tmp_dist
+      dists[j, i] = tmp_dist
     end
   end
   return dists
@@ -67,7 +59,7 @@ function similarities(D, m, n, tol, perp, epsilon)
   h_target = log(perp)
 
   # the similarity matrix that we're going to populate
-  P = [[Float64(0) for i in 1:n] for j in 1:n]
+  P = zeros(n, n)
 
   # work through all of the rows
   for i in 1:n
@@ -84,10 +76,10 @@ function similarities(D, m, n, tol, perp, epsilon)
       for k in 1:m
         p = 0
         if i != k
-          p = exp(-D[i][k]*beta)
+          p = exp(-D[i, k]*beta)
         end
 
-        P[i][k] = p # note that we're not updating the whole thing (can't really do shorthand as a result)
+        P[i, k] = p # note that we're not updating the whole thing (can't really do shorthand as a result)
         p_sum += p
       end
       
@@ -96,10 +88,10 @@ function similarities(D, m, n, tol, perp, epsilon)
       for k in 1:m
         p = 0
         if p_sum != 0
-          p = P[i][k]/p_sum
+          p = P[i, k]/p_sum
         end
 
-        P[i][k] = p
+        P[i, k] = p
         if p > epsilon
           h -= p*log(p)
         end
@@ -137,21 +129,51 @@ function similarities(D, m, n, tol, perp, epsilon)
 end
 
 
-function cost_gradient(P, Y, n, m_out, P_log_P, fake_zero)
+function cost_gradient(P, Y, dCdY, n, m_out, P_log_P, fake_zero)
   # q i|j (1+dist^2)^-1 / sum(i≠j, (1+dist^2)^-1)
   Y_dist = squared_distances(Y, n, m_out)
   # set up the top part of that calculation
-  Q_top = [[((i != j) ? 1/(1+Y_dist[i][j]) : 0) for j in 1:n] for i in 1:n] 
+  Q_top = zeros(n, n)
+  for i in 1:n
+    for j in 1:n
+      if i!=j
+        Q_top[i, j] = 1/(1+Y_dist[i, j])
+      end
+    end
+  end
   # get the bottom bit of that function
   sum_q = sum(sum(Q_top))
   # build up the q i|j set
-  Q = [[max(Q_top[i][j]/sum_q, fake_zero) for j in 1:n] for i in 1:n]
+  Q = zeros(n, n)
+  for i in 1:n
+    for j in 1:n
+      Q[i, j] = max(Q_top[i, j]/sum_q, fake_zero)
+    end
+  end
   # the non-constant portion of the divergence
-  Q_log = [[log(Q[i][j]) for j in 1:n] for i in 1:n]
+  Q_log = zeros(n, n)
+  for i in 1:n
+    for j in 1:n
+      Q_log[i, j] = log(Q[i, j])
+    end
+  end
   # compute the Kullback-Leiber divergence
-  P_log_Q = sum(sum([[P[i][j]*Q_log[i][j] for j in 1:n] for i in 1:n]))
+  P_log_Q = sum(sum(P*Q_log))
   # work out the gradient of the Kullback-Leibler divergence between P and the Student-t based joint probability distribution Q
-  return [4*sum([(P[i][j]-Q[i][j])*(Y[i]-Y[j])/(1+Y_dist[i][j]) for j in 1:n]) for i in 1:n]
+  mult = P-Q
+  mult = mult.*4
+  mult = mult*Y_dist
+  for r in 1:n
+    for c in 1:n
+      m = mult[r, c]
+      for k in 1:m_out
+        y_diff = Y[r, k] - Y[c, k]
+        dCdY[r, k] += m*y_diff
+      end
+    end
+  end
+  return dCdY
+  #return [4*sum([(P[i][j]-Q[i][j])*(Y[i]-Y[j])/(1+Y_dist[i][j]) for j in 1:n]) for i in 1:n]
 end
 
 
@@ -162,38 +184,63 @@ println("Distances calculated")
 P = similarities(D, m, n, tol, perp, epsilon)
 println("Similarities calculated")
 # set p ij = (p j|i + p i|j)/2n
-P = [[max((P[j][i]+P[i][j])/(2*n), fake_zero) for i in 1:n] for j in 1:n]
+P = zeros(n, n)
+for i in 1:n
+  for j in 1:m_out
+    P[i, j] = max((P[j, i]+P[i, j])/(2*n), fake_zero)
+  end
+end
 println("Similarities adjusted")
 # sample initial solution Y(0)={y1, y2, ..., yn} for N(0,10^-4*I)
 init_vals = rand(Normal(0, init_stdev), m_out*n)
-Y = [[init_vals[m_out*i-j+1] for j in 1:m_out] for i in 1:n]
-Y_2 = Y # the Y(t-2) set that is to be used for the momentum calculations
+Y = zeros(n, m_out)
+for i in 1:n
+  for j in 1:m_out
+    Y[i, j] = init_vals[m_out*i-j+1]
+  end
+end
+dCdY = zeros(n, m_out)
 println("Y initialised")
 # compute and store the constant portion of the KL divergence
-P_log_P = sum(sum([[log(P[i][j])*P[i][j] for i in 1:n] for j in 1:n]))
+P_log_P = zeros(n, n)
+for i in 1:n
+  for j in 1:n
+    P_log_P[i, j] = log(P[i, j])*P[i, j] 
+  end
+end
+P_log_P = sum(sum(P_log_P))
 # for t=1 to T do
 #
 #   where T is the number of epochs that we're looking to work through
 println("Training")
 for t in 1:T
-  global P, Y, Y_2, n, m_out, P_log_P, fake_zero, cols, momentum
+  global P, Y, n, m_out, P_log_P, fake_zero, cols, momentum, dCdY
   # compute the gradient matrix
-  dCdY = cost_gradient(P, Y, n, m_out, P_log_P, fake_zero)
-  # saving the Y(t-1) matrix for the time being
-  Y_1 = Y
-  # step in the direction of the gradient (times the learning rate)
-  Y += dCdY.*l_rate+(Y-Y_2).*momentum
+  dCdY = cost_gradient(P, Y, dCdY, n, m_out, P_log_P, fake_zero)
+  # apply scaled gradient
+  println(Y[1:5, :])
+  Y -= dCdY.*l_rate
+  # reproject Y to have zero mean
+  Y_mean = zeros(m_out)
+  for i in 1:n
+    for j in 1:m_out
+      Y_mean[j] += Y[i, j]
+    end
+  end
 
-  # Y has been updated - our Y(t-1) can now be used as Y(t-2) for the next iter
-  Y_2 = Y_1
+  for i in 1:n
+    for j in 1:m_out
+      Y[i, j] -= (Y_mean[j]/n)
+    end
+  end
+  println(Y[1:5, :])
   println(t)
   if t%10 == 0 # keeping myself from getting swamped in charts
-    xs = [[Y[i][1]] for i in 1:n]
-    ys = [[Y[i][2]] for i in 1:n]
-    
-    p = scatter(xs[cols .== 1], ys[cols .== 1], markercolor=colmap[1])
-    for k = 2:length(colmap)
-        scatter!(xs[cols .== k], ys[cols .== k], markercolor=colmap[k])
+    indices = [i for i in 1:n if cols[i]==unique_cols[1]]
+    p = scatter(Y[:, 1][indices], Y[:, 2][indices], markercolor=colmap[1])
+    for k = 2:length(unique_cols)
+      indices = [i for i in 1:n if cols[i]==unique_cols[k]]
+      scatter!(Y[:, 1][indices], Y[:, 2][indices], markercolor=colmap[k])
     end
     display(p)
   end
